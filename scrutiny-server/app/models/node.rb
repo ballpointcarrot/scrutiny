a5a2b23049cf935740a3b6f7ec8497181928aad2
Christopher Kruse <kruse.christopher@gmail.com>
2012-08-22 21:51:30 -0700

    removing rails project

--- scrutiny-server/app/models/node.rb
+++
@@ -1,5 +0,0 @@
-class Node < ActiveRecord::Base
-  attr_accessible :name, :key
-
-  validates_presence_of :name, :key
-end
--- scrutiny-server/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ScrutinyServer::Application.config.secret_token = '3631b2bece5556b50f2795e3660dca3c8fb11bb5a800f038e7b5d9d7164a783023a08ac743900f40bffa84914d27f6f96f78f185e8df978d87035d5d929aa0f4'
